7736bc3e13567abb34e921336b07176ca06243cb
VitalyDychko <Dartveider233@gmail.com>
2021-12-03 00:50:41 +0500

    Задача #2

---
+++ Form2.Designer.cs
@@ -0,0 +1,64 @@
+﻿
+namespace IS_3_19_DYCHKOVI
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.Location = new System.Drawing.Point(-1, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(156, 137);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Подключение к базе данных";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(154, 137);
+            this.Controls.Add(this.button1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace IS_3_19_DYCHKOVI
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        class ConnectionToMySQL
+        {
+            public MySqlConnection ConnectionDataBase()
+            {
+                string ConnStr = "server=caseum.ru;port=33333;user=test_user;database=db_test;password=test_pass;";
+                MySqlConnection conn = new MySqlConnection(ConnStr);
+                return conn;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ConnectionToMySQL CtMSQL = new ConnectionToMySQL();
+            try
+            {
+                CtMSQL.ConnectionDataBase().Open();
+            }
+            catch(Exception oshibka)
+            {
+                MessageBox.Show($"{oshibka}");
+            }
+            finally
+            {
+                MessageBox.Show("Вы успешно подключились к базе данных!");
+                CtMSQL.ConnectionDataBase().Close();
+            }
+        }
+    }
+}
