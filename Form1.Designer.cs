2957301a18075a94a2e53b049ff0a0f746b0c867
VitalyDychko <Dartveider233@gmail.com>
2021-12-03 00:22:38 +0500

    Задача #1

---
+++ Form1.Designer.cs
@@ -0,0 +1,334 @@
+﻿
+namespace IS_3_19_DYCHKOVI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(272, 238);
+            this.listBox1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(430, 43);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(148, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(430, 69);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(148, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
+            this.button2.Location = new System.Drawing.Point(12, 261);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(272, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Центральный процессор";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(430, 144);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(148, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(430, 170);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(148, 20);
+            this.textBox4.TabIndex = 7;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(430, 196);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(148, 20);
+            this.textBox5.TabIndex = 8;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
+            this.button3.Location = new System.Drawing.Point(12, 290);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(272, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Видеокарта";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(430, 259);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(148, 20);
+            this.textBox6.TabIndex = 9;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(430, 285);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(148, 20);
+            this.textBox7.TabIndex = 10;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(430, 311);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(148, 20);
+            this.textBox8.TabIndex = 11;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(430, 95);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(148, 20);
+            this.textBox9.TabIndex = 12;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(289, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 18);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Год";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(289, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 18);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Цена";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold);
+            this.label3.Location = new System.Drawing.Point(289, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 18);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Артикул";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F, System.Drawing.FontStyle.Bold);
+            this.label4.Location = new System.Drawing.Point(321, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(169, 25);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Комплектация";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold);
+            this.label5.Location = new System.Drawing.Point(290, 146);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 18);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "Частота";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold);
+            this.label6.Location = new System.Drawing.Point(290, 172);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(106, 18);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "Кол-во ядер";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold);
+            this.label7.Location = new System.Drawing.Point(290, 198);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(134, 18);
+            this.label7.TabIndex = 20;
+            this.label7.Text = "Кол-во потоков";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold);
+            this.label8.Location = new System.Drawing.Point(290, 261);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(113, 18);
+            this.label8.TabIndex = 21;
+            this.label8.Text = "Частота GPU";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold);
+            this.label9.Location = new System.Drawing.Point(290, 287);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(131, 18);
+            this.label9.TabIndex = 22;
+            this.label9.Text = "Производитель";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold);
+            this.label10.Location = new System.Drawing.Point(290, 313);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(67, 18);
+            this.label10.TabIndex = 23;
+            this.label10.Text = "Память";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.25F, System.Drawing.FontStyle.Bold);
+            this.label11.Location = new System.Drawing.Point(322, 119);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(241, 22);
+            this.label11.TabIndex = 24;
+            this.label11.Text = "Центральный Процессор";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F, System.Drawing.FontStyle.Bold);
+            this.label12.Location = new System.Drawing.Point(321, 225);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(141, 25);
+            this.label12.TabIndex = 25;
+            this.label12.Text = "Видеокарта";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(590, 352);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        public System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IS_3_19_DYCHKOVI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        abstract public class Accessories<T>
+        {
+            public T vendor { get; set; }
+            public int year;
+            public int price;
+            public Accessories(T Vendor, int Year, int Price)
+            {
+                vendor = Vendor;
+                year = Year;
+                price = Price;
+            }
+            public abstract void Display(ListBox lb1);
+        }
+        class CentProc<T> : Accessories<T>
+        {
+            private int Freak { get; set; }
+            private int Kernels { get; set; }
+            private int Threads { get; set; }
+            public CentProc(int freak, int kernels, int threads, T Vendor, int Year, int Price) : base(Vendor, Year, Price) { Freak = freak; Kernels = kernels; Threads = threads; }
+            public override void Display(ListBox lb1)
+            {
+                lb1.Items.Add("#################################");
+                lb1.Items.Add($"Артикул: {vendor}");
+                lb1.Items.Add($"Цена: {price}");
+                lb1.Items.Add($"Год: {year}");
+                lb1.Items.Add("#################################");
+            }
+        }
+        class VideoCard<T> : Accessories<T>
+        {
+            private int GPU { get; set; }
+            private string Developer { get; set; }
+            private int Memory { get; set; }
+            public VideoCard(int gpu, string developer, int memory, T Vendor, int Year, int Price) : base(Vendor, Year, Price) { GPU = gpu; Developer = developer; Memory = memory; }
+            public override void Display(ListBox lb1)
+            {
+                lb1.Items.Add("#################################");
+                lb1.Items.Add($"Артикул: {vendor}");
+                lb1.Items.Add($"Цена: {price}");
+                lb1.Items.Add($"Год: {year}");
+                lb1.Items.Add($"GPU частота: {GPU}");
+                lb1.Items.Add($"Производитель: {Developer}");
+                lb1.Items.Add($"Память: {Memory}");
+                lb1.Items.Add("#################################");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string vend = Convert.ToString(textBox9.Text);
+            int yer = Convert.ToInt32(textBox1.Text);
+            int pric = Convert.ToInt32(textBox2.Text);
+            int freaq = Convert.ToInt32(textBox3.Text);
+            int core = Convert.ToInt32(textBox4.Text);
+            int thread = Convert.ToInt32(textBox5.Text); 
+            CentProc<string> CP = new CentProc<string>(Convert.ToInt32(yer), Convert.ToInt32(pric), Convert.ToInt32(freaq), Convert.ToString(vend), Convert.ToInt32(core), Convert.ToInt32(thread));
+            CP.Display(listBox1);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string vend = Convert.ToString(textBox9.Text);
+            int yer = Convert.ToInt32(textBox1.Text);
+            int pric = Convert.ToInt32(textBox2.Text);
+            int gpu2 = Convert.ToInt32(textBox6.Text);
+            string dev = Convert.ToString(textBox7.Text);
+            int memo = Convert.ToInt32(textBox8.Text);
+            VideoCard<string> VC = new VideoCard<string>(Convert.ToInt32(gpu2), Convert.ToString(dev), Convert.ToInt32(memo), Convert.ToString(vend), Convert.ToInt32(yer), Convert.ToInt32(pric));
+            VC.Display(listBox1);
+        }
+    }
+}
