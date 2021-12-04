da1d8249813b26a4489726c3a0f64da75a4ce115
ВИТАЛИЙ Дычко <Dartveider233@gmail.com>
2021-12-04 12:34:52 +0500

    Переадресация библиотеки в корневую папку проекта

--- Form4.cs
+++ Form4.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using MySql.Data.MySqlClient;
-using GreatLibrary;
+using GreatLibrarium;
 
 namespace IS_3_19_DYCHKOVI
 {
